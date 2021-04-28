89698ac6a2fa783fd441baa73133dc689b4b2018
batiscuff <batiscuff@protonmail.com>
2021-04-28 13:13:53 +0300

    loadProxies migrate to boostutils & added flags

--- boosterutils/boosterutils.go
+++ boosterutils/boosterutils.go
@@ -1,6 +1,13 @@
 package boosterutils
 
-import "net/url"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+)
 
 func IsValidURL(inputUrl string) bool {
 	_, err := url.ParseRequestURI(inputUrl)
@@ -12,4 +19,39 @@ func IsValidURL(inputUrl string) bool {
 		return false
 	}
 	return true
+}
+
+func loadProxies(fileName string) (proxies []string, err error) {
+	var body io.Reader
+	// Загрузка прокси из линка и из файла
+	if IsValidURL(fileName) {
+		resp, err := http.Get(fileName)
+		if err != nil {
+			return nil, fmt.Errorf("Proxies from link not loaded! %q", err)
+		}
+		defer resp.Body.Close()
+		body = resp.Body
+	} else {
+		fileObj, err := os.Open(fileName)
+		if err != nil {
+			return nil, fmt.Errorf("Proxies from file not loaded! %q", err)
+		}
+		defer fileObj.Close()
+		body = fileObj
+	}
+
+	var lines []string
+	scanner := bufio.NewScanner(body)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if len(line) != 0 { // index out of range [0] with length 0
+			if IsValidURL(line) {
+				lines = append(lines, line)
+			} else {
+				line = "http://" + line
+				lines = append(lines, line)
+			}
+		}
+	}
+	return lines, scanner.Err()
 }
--- main.go
+++ main.go
@@ -1,16 +1,13 @@
 package main
 
 import (
-	"bufio"
+	"flag"
 	"fmt"
-	"io"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"net/url"
 	"os"
 	"regexp"
-	"strconv"
 	"time"
 
 	utils "github.com/batiscuff/tg_booster/boosterutils"
@@ -19,8 +16,13 @@ import (
 	"github.com/gammazero/workerpool"
 )
 
-func addView(proxy string) {
+func addView(proxy string, link string) {
 	// Создание клиента и юзер-агента, добавление таймаута и прокси
+	if utils.IsValidURL(link) != true {
+		fmt.Println("Invalid post link! Example link: https://t.me/channel_name/1")
+		os.Exit(1)
+	}
+
 	proxyUrl, err := url.Parse(proxy)
 	ua := browser.Computer()
 	client := &http.Client{
@@ -29,8 +31,8 @@ func addView(proxy string) {
 	}
 
 	// Создание и конфигурация запроса
-	requestUrl := "https://t.me/parsing_conf/108?embed=1"
-	request, err := http.NewRequest("GET", requestUrl, nil)
+	link = link + "?embed=1"
+	request, err := http.NewRequest("GET", link, nil)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -42,6 +44,7 @@ func addView(proxy string) {
 		fmt.Println(err)
 	} else {
 		// Вывод инфы в консоль
+		defer response.Body.Close()
 		body, err := ioutil.ReadAll(response.Body)
 		if err != nil {
 			fmt.Printf("Error reading HTTP body. %q\n", err)
@@ -53,7 +56,6 @@ func addView(proxy string) {
 		if re.Match([]byte(body)) {
 			dataViewString = string(re.FindSubmatch(body)[1])
 		}
-		response.Body.Close()
 	}
 
 	if response != nil && response.StatusCode == 200 {
@@ -67,83 +69,38 @@ func addView(proxy string) {
 			request.AddCookie(response.Cookies()[0])
 		}
 		request.Header.Set("X-Requested-With", "XMLHttpRequest")
-		request.Header.Set("Referer", requestUrl)
+		request.Header.Set("Referer", link)
 		request.Header.Set("User-Agent", ua)
 
 		response, err = client.Do(request)
 		if err != nil {
 			fmt.Println(err)
 		} else {
+			defer response.Body.Close()
 			fmt.Printf("Response status code: %d\n", response.StatusCode)
-			response.Body.Close()
 		}
 
 	}
-}
-
-
-func loadProxies() (proxies []string, err error) {
-	// var fileObj os.Stdin
-	var body io.Reader
-	if len(os.Args) <= 1 || (os.Args[1] == "-h" || os.Args[1] == "--help") {
-		err = fmt.Errorf("Запуск приложения возможен с названием файла или ссылкой на прокси!")
-		return nil, err
-	} else if len(os.Args) >= 2 {
-		filename := os.Args[1]
-		// Загрузка прокси из линка и из файла
-		if utils.IsValidURL(filename) {
-			resp, err := http.Get(filename)
-			if err != nil {
-				return nil, fmt.Errorf("Прокси из ссылки небыли загружены!")
-			}
-			defer resp.Body.Close()
-			body = resp.Body
-		} else {
-			fileObj, err := os.Open(filename)
-			if err != nil {
-				log.Fatal(err)
-			}
-			defer fileObj.Close()
-			body = fileObj
-		}
-	}
-
-	var lines []string
-	scanner := bufio.NewScanner(body)
-	for scanner.Scan() {
-		line := scanner.Text()
-		if len(line) != 0 { // index out of range [0] with length 0
-			if utils.IsValidURL(line) {
-				lines = append(lines, line)
-			} else {
-				line = "http://" + line
-				lines = append(lines, line)
-			}
-		}
-	}
-	return lines, scanner.Err()
 }
 
 func main() {
-	proxies, err := loadProxies()
+	fileName := flag.String("p", "", "Proxies file or link with http proxies ended .txt")
+	workers := flag.Int("w", 50, "Count of workers on pool")
+	postLink := flag.String("l", "", "Link on Telegram post for boost views. https://t.me/...")
+	flag.Parse()
+
+	proxies, err := utils.loadProxies(*fileName)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
 	// --- Workers Pool --
-	var workers int
-	if len(os.Args) == 3 {
-		workers, err = strconv.Atoi(os.Args[2])
-	} else {
-		workers = 100
-	}
-
-	wp := workerpool.New(workers)
+	wp := workerpool.New(*workers)
 	for _, proxy := range proxies {
 		proxy := proxy
 		wp.Submit(func() {
-			addView(proxy)
+			addView(proxy, *postLink)
 		})
 	}
 	wp.StopWait()
